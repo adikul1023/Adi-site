feb73cdd2756c0f5d8e067131ad83d8fef3c4fe1
Adi K <102898613+adikul1023@users.noreply.github.com>
2024-10-18 19:33:50 +0530

    Update Main.js

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -11,7 +11,7 @@ const Main = (props) => (
   <HelmetProvider>
     <Analytics />
     <ScrollToTop />
-    <Helmet titleTemplate="%s | Brandon Donelan" defaultTitle="Brandon Donelan">
+    <Helmet titleTemplate="%s | Adi Kulkarni" defaultTitle="Adi Kulkarni">
       {props.title && <title>{props.title}</title>}
       <meta name="description" content={props.description} />
     </Helmet>
@@ -39,7 +39,7 @@ Main.defaultProps = {
   children: null,
   fullPage: false,
   title: null,
-  description: "Brandon Donelan's personal website.",
+  description: "Adi Kulkarni's personal website.",
 };
 
 export default Main;
